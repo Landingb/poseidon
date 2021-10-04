4f451f961c52c1815d8fddec1dbab61117557a6d
Lunaesky <landing.badji@outlook.com>
2021-10-04 17:35:08 +0200

    test v1

--- src/test/java/com/nnk/springboot/RatingTests.java
+++ src/test/java/com/nnk/springboot/RatingTests.java
@@ -2,6 +2,7 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.repositories.RatingRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,23 +26,61 @@ public class RatingTests {
 		Rating rating = new Rating("Moodys Rating", "Sand PRating", "Fitch Rating", 10);
 
 		// Save
-		rating = ratingRepository.save(rating);
+		rating = ratingService.save(rating);
 		Assert.assertNotNull(rating.getId());
 		Assert.assertTrue(rating.getOrderNumber() == 10);
 
 		// Update
 		rating.setOrderNumber(20);
-		rating = ratingRepository.save(rating);
+		rating = ratingService.save(rating);
 		Assert.assertTrue(rating.getOrderNumber() == 20);
 
 		// Find
-		List<Rating> listResult = ratingRepository.findAll();
+		List<Rating> listResult = ratingService.findAll();
 		Assert.assertTrue(listResult.size() > 0);
 
 		// Delete
 		Integer id = rating.getId();
-		ratingRepository.delete(rating);
-		Optional<Rating> ratingList = ratingRepository.findById(id);
-		Assert.assertFalse(ratingList.isPresent());
+		ratingService.delete(id);
+		Rating ratingList = ratingService.findById(id);
+		Assert.assertNull(ratingList);
+	}
+
+	@Test
+	public void ratingConstraintsTest_shouldThrownConstraintViolationException() {
+
+		Rating rating1 = new Rating("test", "test", "", 1);
+		Rating rating2 = new Rating("" ,"test" ,"test" ,1);
+		Rating rating3 = new Rating("test" ,"" ,"test" ,1);
+		Rating rating4 = new Rating("test" ,"test" ,"test" ,null);
+
+		Assertions.assertThatExceptionOfType(ConstraintViolationException.class).isThrownBy( () -> ratingService.save(rating1));
+		Assertions.assertThatExceptionOfType(ConstraintViolationException.class).isThrownBy( () -> ratingService.save(rating2));
+		Assertions.assertThatExceptionOfType(ConstraintViolationException.class).isThrownBy( () -> ratingService.save(rating3));
+		Assertions.assertThatExceptionOfType(ConstraintViolationException.class).isThrownBy( () -> ratingService.save(rating4));
+	}
+
+	@Test
+	public void ratingSaveAttrsTest_shouldSaveCorrectlyAllAttrs() {
+		String moodysRating = "Moodys Test";
+		String sandPRating = "SandP Test";
+		String fitchRating = "Fitch Test";
+		Integer orderNumber = 1;
+
+		Rating rating = new Rating();
+		rating.setMoodysRating(moodysRating);
+		rating.setSandPRating(sandPRating);
+		rating.setFitchRating(fitchRating);
+		rating.setOrderNumber(orderNumber);
+
+		Rating savedRating = ratingService.save(rating);
+
+		Assert.assertNotNull(savedRating.getId());
+		Assert.assertEquals("moodysRating", moodysRating, savedRating.getMoodysRating());
+		Assert.assertEquals("sandPRating", sandPRating, savedRating.getSandPRating());
+		Assert.assertEquals("fitchRating", fitchRating, savedRating.getFitchRating());
+		Assert.assertEquals("orderNumber", orderNumber, savedRating.getOrderNumber());
+
+		ratingService.delete(savedRating.getId());
 	}
 }
