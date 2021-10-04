3a461ebcb926d98e0446915d379e45a72e628905
Lunaesky <landing.badji@outlook.com>
2021-10-04 18:40:58 +0200

    add files

---
+++ src/main/java/com/nnk/springboot/exceptions/PasswordPatternException.java
@@ -0,0 +1,12 @@
+package com.nnk.springboot.exceptions;
+
+public class PasswordPatternException extends Exception {
+
+    public PasswordPatternException() {
+        super();
+    }
+
+    public PasswordPatternException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/nnk/springboot/exceptions/UsernameExistException.java
@@ -0,0 +1,12 @@
+package com.nnk.springboot.exceptions;
+
+public class UsernameExistException extends Exception {
+
+    public UsernameExistException() {
+        super();
+    }
+
+    public UsernameExistException(String s) {
+        super(s);
+    }
+}
